9effae648c4db4fae1bf1f6e21bd1706c1449a89
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2025-05-29 13:29:07 +0300

    Exception handling

--- modules/markdown_reader/Reader.py
+++ modules/markdown_reader/Reader.py
@@ -20,16 +20,19 @@ class Md_reader:
         return content
 
     def get_md_files(self):
-        path = self.get_full_path()
-        md_files = Path(path).rglob("*.md")
-        for md_file in md_files:
-            file_name = md_file.resolve().name
-            file_route = md_file.resolve().as_posix()
-            file_content = self.read_a_single_file(file_route)
-            ob = {
-            "file_name":file_name,
-            "file_route":file_route,
-            "file_content":file_content   
-            }
-            self.content_list.append(ob)
-        print(self.content_list)
+        try:
+            path = self.get_full_path()
+            md_files = Path(path).rglob("*.md")
+            for md_file in md_files:
+                file_name = md_file.resolve().name
+                file_route = md_file.resolve().as_posix()
+                file_content = self.read_a_single_file(file_route)
+                ob = {
+                "file_name":file_name,
+                "file_route":file_route,
+                "file_content":file_content   
+                }
+                self.content_list.append(ob)
+            print(self.content_list)
+        except Exception as e:
+            print(f"Exception: {e}")
