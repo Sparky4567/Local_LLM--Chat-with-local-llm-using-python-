2e64f964d0b6631e512f5ad5601a4321d8c39286
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2025-05-29 13:55:30 +0300

    Example

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,6 @@
 #Change the value of this variable to your preferred model name
 #Maybe llama3 or anything you want it to be, as long as you have it on your machine
-DEFAULT_LLM_MODEL = "tinyllama"
+
+
+#DEFAULT_LLM_MODEL = "tinyllama"
+DEFAULT_LLM_MODEL = "qwen3:0.6b"
